ad8b50d4a9c7f296d46c416b10e50b222d342d1c
kstarzecki <k.starzecki@gmail.com>
2021-04-15 11:24:48 +0200

    improved description export, 'fixed' bibs and bobs, cleaned up

--- exass.js
+++ exass.js
@@ -11,23 +11,31 @@ const url = process.env.CANVAS_API_URL_TEST
 const canvas = Canvas(url, token)
 
 const config = {
-  account: '',
+  account: '', // for later
   courseId: '26682',
   assignmentId: '146181'
 }
 
 const dir = './Export/'
+const aDir = './Export/Attachments/'
 
 const parsedAssignments = []
 
+function handleDirectory (dir) {
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+  }
+}
+
 function reStripAndReconstruct (re, arr, oarr) {
   arr.forEach(element => {
-    // var dir = '/files/'
-    if (re.exec(element) !== null) {
-      oarr.push(re.exec(element)[1])
+    try {
+      if (re.exec(element) !== null) {
+        oarr.push(re.exec(element)[1])
+      }
+    } catch (e) {
+      console.error('CAUGHT AN ERROR: ' + e + '. Continuing...')
     }
-    // else {
-    // }
   })
 }
 
@@ -55,22 +63,26 @@ function createAssignmentObject (name, description, urls, dnames) {
   return shortObject
 }
 
-function saveAssignments (obj) {
+function saveAssignments (arr) {
   try {
-    fs.writeFileSync(`${dir}test.txt`, JSON.stringify(obj))
-  } catch (err) {
-    console.error(err)
+    arr.forEach(element => {
+      const path = `${dir}${element.name}.txt`
+      const buffer = Buffer.from(element.description)
+      fs.open(path, 'w', function (e, fd) {
+        if (e) {
+          console.error(`Could not open file: ${e}`)
+        }
+        fs.write(fd, buffer, 0, buffer.length, null, function (e) {
+          if (e) console.error(`could not write file: ${e}`)
+          fs.close(fd, function () {
+            console.log(`Successfully wrote "${element.name}.txt" to ${dir}.`)
+          })
+        })
+      })
+    })
+  } catch (e) {
+    console.error('CAUGHT AN ERROR: ' + e + '. Continuing...')
   }
-  // obj.forEach(element => {
-  //   fs.open(`Assignments/${obj.name}.txt`, 'w', function( e, id ) {
-  //     fs.write( id, `hello`, null, 'utf8', function(){
-  //       // `${element.description}` + "\n" + `${element.uniqueUrls}` + "\n" + `${element.uniqueNames}`
-  //       fs.close(id, function(){
-  //       console.log(`${obj.name} Saved.`)
-  //       })
-  //     })
-  //   })
-  // })
 }
 
 async function getAssignment (config, arr) {
@@ -88,7 +100,7 @@ async function getAssignment (config, arr) {
     var dnames = await processFiles(dlist, parsedAssignments)
     arr.push(createAssignmentObject(assignment.body.name, clean, dlist, dnames))
   } catch (e) {
-    console.log('CAUGHT AN ERROR: ' + e + '. Continuing...')
+    console.error('CAUGHT AN ERROR: ' + e + '. Continuing...')
   }
 }
 
@@ -107,26 +119,26 @@ async function processFiles (arr) {
 async function downloadFile (dname, durl, dlock, dlockExp, did) {
   if (dlock === false) {
     const downloadStream = got.stream(durl)
-    const fileWriterStream = createWriteStream(`${dir}${did}_${dname}`)
+    const fileWriterStream = createWriteStream(`${aDir}${did}_${dname}`)
 
     await downloadStream
-      .on('error', (error) => {
-        console.error(`Download failed: ${error.message}`)
+      .on('error', (e) => {
+        console.error(`Download failed: ${e.message}`)
       })
 
     fileWriterStream
-      .on('error', (error) => {
-        console.error(`Could not write file "${did}_${dname}" to system: ${error.message}`)
+      .on('error', (e) => {
+        console.error(`Could not write file "${did}_${dname}" to system: ${e.message}`)
       })
       .on('finish', () => {
-        console.log(`File "${did}_${dname}" downloaded to ${dir}`)
+        console.log(`File "${did}_${dname}" downloaded to ${aDir}`)
       })
 
     await downloadStream.pipe(fileWriterStream)
   } else {
-    console.log(`File "${dname}" ID: ${did} is locked. Skipping...`)
-    fs.open(`${dir}lockedFiles.txt`, 'w', function (e, id) {
-      fs.write(id, `File "${dname}" with ID: ${did} is locked. Reason: ${dlockExp}` + '\n', null, 'utf8', function () {
+    console.log(`File "${dname}" ID: ${did} is locked. Continuing...`)
+    fs.open(`${aDir}lockedFiles.txt`, 'w', function (e, id) {
+      fs.write(id, `Failed to download file "${dname}" with ID: ${did}. Reason: ${dlockExp}` + '\n', null, 'utf8', function () {
         fs.close(id, function () {
           console.log('Lock file is updated.')
         })
@@ -136,9 +148,8 @@ async function downloadFile (dname, durl, dlock, dlockExp, did) {
 }
 
 async function start () {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir)
-  }
+  handleDirectory(dir)
+  handleDirectory(aDir)
   await getAssignment(config, parsedAssignments)
   saveAssignments(parsedAssignments)
 }
